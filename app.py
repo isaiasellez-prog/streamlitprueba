d223ffc29e96cd8e9751e93da60723e23221853d
isaiasellez-prog <isaias.ellez@gmail.com>
2026-02-24 17:14:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Prueba COSTOS_SALUD")
-st.write("Hola Isaías — primera app Streamlit")
+st.title("Consulta COSTOS_SALUD")
+
+data = pd.DataFrame({
+    "Periodo": [2024, 2024, 2025, 2025],
+    "Division": ["Chuquicamata", "Salvador", "Chuquicamata", "Andina"],
+    "Costo": [120000, 300000, 150000, 200000]
+})
+
+division = st.selectbox("División", data["Division"].unique())
+
+df = data[data["Division"] == division]
+
+st.dataframe(df)
